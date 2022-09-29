db634a0f6d5ecbdaaff7eaa4fa0695a5f409a2a8
Daisy <90488153+DaisyRajasankar@users.noreply.github.com>
2022-09-29 11:10:49 +0530

    Add files via upload

---
+++ Assignments/Team Member 2/alarm.py
@@ -0,0 +1,38 @@
+import random
+import time
+t1 = int(input("Enter the threshold value for temperature from 1C to 100C : "))
+t2 = int(input("Enter the threshold value for humidity 1% to 50% : "))
+
+h = 0
+t = 0
+
+while(True):
+    temp = random.uniform(1, 100)
+    humidity = random.uniform(1,50)
+    print("Temperature is : {0:.2f}".format(temp))
+    print("Humidity is : {0:.2f} %".format(humidity))
+    time.sleep(1)
+    
+    if temp >= t1 and humidity<= t2:
+        h =1
+        t = 1
+        print("HIGH TEMPERATURE\nHumidity alarm and Temperature turn ON")
+        time.sleep(2)
+        
+    elif humidity <= t2:
+        h = 1
+        t = 0
+        print("Humidity alarm is ON and Temperature alarm is OFF")
+        time.sleep(2)
+
+    elif temp >= t1:
+        h=0
+        t = 1
+        print("Humidity alarm is OFF and Temperature alarm is ON")
+        time.sleep(2)
+
+    else:
+        h = 0
+        t = 0
+        print("Both alarms are OFF")
+        time.sleep(2)
